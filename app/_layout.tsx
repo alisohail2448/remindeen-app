b41bc31357578e0f4fe56f9667b2be56877346e2
Sohail Akhtar Ali <sohail@truscholar.io>
2024-08-14 12:08:29 +0530

    Sohail | feat(screens): added starting and login screen

---
+++ app/_layout.tsx
@@ -0,0 +1,20 @@
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  useFonts({
+    redhat: require("./../assets/fonts/RedHatDisplay-Regular.ttf"),
+    "redhat-medium": require("./../assets/fonts/RedHatDisplay-Medium.ttf"),
+    "redhat-bold": require("./../assets/fonts/RedHatDisplay-Bold.ttf"),
+  });
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
+}
---
+++ app/auth/sign-in/index.tsx
@@ -0,0 +1,123 @@
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import React, { useEffect } from "react";
+import { useNavigation, useRouter } from "expo-router";
+import { Colors } from "@/constants/Colors";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+export default function Index() {
+  const navigation = useNavigation();
+  const router = useRouter();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <View
+      style={{
+        backgroundColor: "#fff",
+        height: "100%",
+        padding: 25,
+      }}
+    >
+      <View style={{ marginTop: 100 }}>
+        <Text
+          style={{
+            fontSize: 26,
+            fontFamily: "redhat-bold",
+            color: Colors.primary,
+          }}
+        >
+          Lets Sign You In
+        </Text>
+        <Text style={{ fontSize: 26, fontFamily: "redhat", marginTop: 20 }}>
+          Welcome Back
+        </Text>
+        <Text style={{ fontSize: 26, fontFamily: "redhat", marginTop: 20 }}>
+          You've been missed!
+        </Text>
+      </View>
+
+      <View style={{ marginTop: 40 }}>
+        <Text style={{ fontSize: 16, fontFamily: "redhat", marginBottom: 10 }}>
+          Phone
+        </Text>
+        <TextInput style={styles.input} placeholder="+91 9083427234" />
+      </View>
+
+      <View style={{ marginTop: 20 }}>
+        <Text style={{ fontSize: 16, fontFamily: "redhat", marginBottom: 10 }}>
+          Password
+        </Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter Password"
+          secureTextEntry={true}
+        />
+      </View>
+      <TouchableOpacity style={styles.button}>
+        <Text
+          style={{
+            color: Colors.WHITE,
+            fontFamily: "redhat-medium",
+            textAlign: "center",
+            fontSize: 20,
+          }}
+        >
+          Sign In
+        </Text>
+        <Ionicons
+          name="chevron-forward-outline"
+          size={20}
+          color={Colors.WHITE}
+        />
+      </TouchableOpacity>
+
+      <View>
+        <Text
+          style={{
+            fontSize: 16,
+            fontFamily: "redhat",
+            marginBottom: 10,
+            textAlign: "center",
+            marginTop: 40,
+          }}
+        >
+          Don't have any account?{" "}
+          <Text onPress={() => router.push('/auth/sign-up')} style={{ fontFamily: "redhat-bold", color: Colors.primary }}>
+            Register
+          </Text>
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    padding: 15,
+    borderWidth: 1,
+    borderRadius: 15,
+    borderColor: "#000",
+    fontFamily: "redhat",
+  },
+  button: {
+    padding: 15,
+    width: "100%",
+    backgroundColor: Colors.primary,
+    borderRadius: 10,
+    marginTop: 40,
+    flexDirection: "row",
+    gap: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
