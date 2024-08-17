5d541e6a6ad924c03d93033579f3c355394f5865
Sohail Akhtar Ali <sohail@truscholar.io>
2024-08-17 18:16:55 +0530

    Sohail | feat(profile): profile section ui completed

--- app/profile/index.tsx
+++ app/profile/index.tsx
@@ -8,12 +8,13 @@ import {
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { useNavigation } from "expo-router";
-import { Feather, FontAwesome6 } from "@expo/vector-icons";
+import { Entypo, Feather, FontAwesome6 } from "@expo/vector-icons";
 import { Colors } from "@/constants/Colors";
 import ImageView from "react-native-image-viewing";
 
 export default function Index() {
   const navigation = useNavigation();
+  const [upiCollapse, setUpiCollapse] = useState(false);
   const [visible, setIsVisible] = useState(false);
   useEffect(() => {
     navigation.setOptions({
@@ -21,37 +22,100 @@ export default function Index() {
     });
   }, []);
   return (
-    <View style={{ marginTop: 40, padding: 20 }}>
+    <View
+      style={{
+        marginTop: 40,
+        padding: 16,
+        backgroundColor: "#FBFBFB",
+        height: "100%",
+        marginBottom: 40,
+      }}
+    >
       <View
         style={{
           flexDirection: "row",
+          justifyContent: "space-between",
           alignItems: "center",
-          gap: 10,
           marginBottom: 20,
         }}
       >
-        <TouchableOpacity
-          onPress={() => navigation.goBack()}
-          style={{ backgroundColor: "#e3eeec", borderRadius: 100, padding: 5 }}
-        >
-          <Feather name="arrow-left" size={24} color="#004B40" />
-        </TouchableOpacity>
-        <Text
+        <View
           style={{
-            fontSize: 20,
-            fontFamily: "inter-bold",
-            color: Colors.primary,
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 10,
           }}
         >
-          Profile
-        </Text>
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            style={{
+              backgroundColor: "#e3eeec",
+              borderRadius: 100,
+              padding: 5,
+            }}
+          >
+            <Feather name="arrow-left" size={24} color="#004B40" />
+          </TouchableOpacity>
+          <Text
+            style={{
+              fontSize: 20,
+              fontFamily: "inter-bold",
+              color: Colors.primary,
+            }}
+          >
+            Profile
+          </Text>
+        </View>
+        <TouchableOpacity
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            backgroundColor: "#e3eeec",
+            padding: 8,
+            paddingHorizontal: 16,
+            borderRadius: 100,
+            gap: 6,
+          }}
+        >
+          <FontAwesome6 name="edit" size={20} color={Colors.primary} />
+          <Text
+            style={{
+              fontSize: 15,
+              fontFamily: "inter-medium",
+              color: Colors.primary,
+            }}
+          >
+            Edit
+          </Text>
+        </TouchableOpacity>
       </View>
       <ScrollView>
-        <View style={{ flexDirection: "row", alignItems: "center", gap: 14 }}>
-          <Image
-            style={{ width: 70, height: 70, borderRadius: 100 }}
-            source={require("../../assets/images/profile.png")}
-          />
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 14,
+            backgroundColor: Colors.WHITE,
+            borderWidth: 2,
+            borderColor: "#eee",
+            borderRadius: 10,
+            padding: 16,
+          }}
+        >
+          <View
+            style={{
+              borderWidth: 2,
+              borderColor: Colors.primary,
+              padding: 5,
+              borderRadius: 100,
+              borderStyle: "dashed",
+            }}
+          >
+            <Image
+              style={{ width: 70, height: 70, borderRadius: 100 }}
+              source={require("../../assets/images/profile.png")}
+            />
+          </View>
           <View style={{ gap: 4 }}>
             <View
               style={{ flexDirection: "row", alignItems: "center", gap: 20 }}
@@ -71,7 +135,7 @@ export default function Index() {
                   fontFamily: "inter-medium",
                   color: Colors.primary,
                   textTransform: "capitalize",
-                  backgroundColor: "#D0DED8",
+                  backgroundColor: "#e3eeec",
                   borderRadius: 12,
                   paddingVertical: 1,
                   paddingHorizontal: 14,
@@ -92,7 +156,28 @@ export default function Index() {
           </View>
         </View>
 
-        <View style={{ marginTop: 20, gap: 15 }}>
+        <View
+          style={{
+            marginTop: 20,
+            gap: 15,
+            backgroundColor: Colors.WHITE,
+            borderWidth: 2,
+            borderColor: "#eee",
+            borderRadius: 10,
+            padding: 16,
+          }}
+        >
+          <View>
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "inter-bold",
+                color: Colors.primary,
+              }}
+            >
+              Basic Details
+            </Text>
+          </View>
           <View style={{ gap: 1 }}>
             <Text
               style={{ fontSize: 14, fontFamily: "inter", color: "#5D8082" }}
@@ -159,8 +244,27 @@ export default function Index() {
           </View>
         </View>
 
-        <View style={{ marginTop: 20, gap: 10 }}>
-          <View style={{ gap: 1 }}>
+        <View
+          style={{
+            marginTop: 20,
+            gap: 15,
+            backgroundColor: Colors.WHITE,
+            borderWidth: 2,
+            borderColor: "#eee",
+            borderRadius: 10,
+            padding: 16,
+            marginBottom: 40,
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => setUpiCollapse(!upiCollapse)}
+            style={{
+              gap: 1,
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
             <Text
               style={{
                 fontSize: 16,
@@ -168,54 +272,79 @@ export default function Index() {
                 color: Colors.primary,
               }}
             >
-              UPI Section
+              UPI Details
             </Text>
-          </View>
-          <View style={{ gap: 4 }}>
-            <Text
-              style={{ fontSize: 14, fontFamily: "inter", color: "#5D8082" }}
-            >
-              UPI Id
-            </Text>
-            <View
-              style={{
-                flexDirection: "row",
-                gap: 10,
-                alignItems: "center",
-              }}
-            >
-              <Text
+            {upiCollapse ? (
+              <Feather name="chevron-up" size={24} color={Colors.primary} />
+            ) : (
+              <Feather name="chevron-down" size={24} color={Colors.primary} />
+            )}
+          </TouchableOpacity>
+          {upiCollapse && (
+            <>
+              <View
                 style={{
-                  fontSize: 16,
-                  fontFamily: "inter-medium",
-                  color: Colors.primary,
-                  backgroundColor: "#D0DED8",
-                  borderRadius: 12,
-                  paddingVertical: 8,
-                  paddingHorizontal: 14,
+                  gap: 4,
                 }}
               >
-                7249048715@ybl
-              </Text>
-              <FontAwesome6 name="copy" size={20} color="black" />
-            </View>
-          </View>
-          <View style={{ gap: 4, marginTop: 10 }}>
-            <Text
-              style={{ fontSize: 14, fontFamily: "inter", color: "#5D8082" }}
-            >
-              UPI QR
-            </Text>
-            <TouchableOpacity onPress={() => setIsVisible(true)} >
-              <Image
-                source={{
-                  uri: "https://qph.cf2.quoracdn.net/main-qimg-6f10dcab91fe9a768c8757381a98e9ae-pjlq",
-                }}
-                style={{ width: 200, height: 200, objectFit: "contain" }}
-              />
-            </TouchableOpacity>
-          </View>
+                <Text
+                  style={{
+                    fontSize: 14,
+                    fontFamily: "inter",
+                    color: "#5D8082",
+                  }}
+                >
+                  UPI Id
+                </Text>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    gap: 10,
+                    alignItems: "center",
+                  }}
+                >
+                  <Text
+                    style={{
+                      fontSize: 16,
+                      fontFamily: "inter-medium",
+                      color: Colors.primary,
+                      backgroundColor: "#e3eeec",
+                      borderRadius: 12,
+                      paddingVertical: 8,
+                      paddingHorizontal: 14,
+                    }}
+                  >
+                    7249048715@ybl
+                  </Text>
+                  <FontAwesome6 name="copy" size={20} color="black" />
+                </View>
+              </View>
+              <View style={{ gap: 4, marginTop: 10 }}>
+                <Text
+                  style={{
+                    fontSize: 14,
+                    fontFamily: "inter",
+                    color: "#5D8082",
+                  }}
+                >
+                  UPI QR
+                </Text>
+                <TouchableOpacity
+                  style={{ justifyContent: "center", alignItems: "center" }}
+                  onPress={() => setIsVisible(true)}
+                >
+                  <Image
+                    source={{
+                      uri: "https://qph.cf2.quoracdn.net/main-qimg-6f10dcab91fe9a768c8757381a98e9ae-pjlq",
+                    }}
+                    style={{ width: 200, height: 200, objectFit: "contain" }}
+                  />
+                </TouchableOpacity>
+              </View>
+            </>
+          )}
         </View>
+
         <ImageView
           images={[
             {
